9dd4da028837d1cb005c9e49bdb1f7c1d143df43
Jesse Kempf <jesse.kempf@doublecrown.co>
2020-03-31 17:31:18 -0700

    Add join3 operator

--- src/Dataflow/Operators.hs
+++ src/Dataflow/Operators.hs
@@ -1,12 +1,29 @@
 module Dataflow.Operators (
-  fanout
+  fanout,
+  join3
 ) where
 
 import           Data.Dynamic        (Typeable)
-import           Dataflow.Primitives (Dataflow, Edge, send)
+import           Dataflow.Primitives (Dataflow, Edge, StateRef, Timestamp,
+                                      Vertex (StatefulVertex), newState,
+                                      registerVertex, send)
 import           Dataflow.Vertices   (statelessVertex)
-import           Prelude             (mapM_, ($))
+import           Prelude             (mapM_, ($), (<$>), (<*>))
 
 
 fanout :: Typeable a => [Edge a] -> Dataflow (Edge a)
 fanout nexts = statelessVertex $ \timestamp x -> mapM_ (\next -> send next timestamp x) nexts
+
+
+join3 :: (Typeable i, Typeable j, Typeable k) =>
+  s
+  -> (StateRef s -> Timestamp -> i -> Dataflow ())
+  -> (StateRef s -> Timestamp -> j -> Dataflow ())
+  -> (StateRef s -> Timestamp -> k -> Dataflow ())
+  -> Dataflow (Edge i, Edge j, Edge k)
+join3 initState callbackI callbackJ callbackK = do
+  stateRef <- newState initState
+
+  (,,) <$> registerVertex (StatefulVertex stateRef callbackI)
+       <*> registerVertex (StatefulVertex stateRef callbackJ)
+       <*> registerVertex (StatefulVertex stateRef callbackK)
