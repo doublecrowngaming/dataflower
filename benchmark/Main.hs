44bcc190189b79cb5c633bc293d743ce41baa326
Jesse Kempf <jesse.kempf@doublecrown.co>
2020-05-06 22:48:46 -0700

    Refine the dataflow benchmark for Dataflow.send

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Criterion.Main
+import           Data.Typeable  (Typeable)
 import           Dataflow
 import           Prelude
 import           Test.Dataflow  (runDataflow)
@@ -9,8 +10,9 @@ import           Test.Dataflow  (runDataflow)
 main :: IO ()
 main = defaultMain [
   bgroup "dataflow" [
-      bench "1,000,000 inputs" $ nfIO (runDataflow passthrough [0..1000000 :: Int])
+      bench "1,000,000 inputs" $ nfIO (runDataflow blackhole [0..1000000 :: Int])
     ]
   ]
   where
-    passthrough next = statelessVertex $ \t x -> send next t x
+    blackhole :: Typeable a => Edge a -> Dataflow (Edge a)
+    blackhole _ = discard
